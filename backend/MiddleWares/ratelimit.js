a33d014cfc10dc6bb8bf9b50afb9dc4d330ada1b
Omar Walid <omo_1712@yahoo.com>
2023-05-04 15:31:07 +0300

    add middlewares

---
+++ backend/MiddleWares/ratelimit.js
@@ -0,0 +1,8 @@
+const rateLimit = require("express-rate-limit");
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+module.exports = apiLimiter;
