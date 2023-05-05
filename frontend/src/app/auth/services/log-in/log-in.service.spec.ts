fe24046a3a0d5fc9bd06184afdf1bdaf61c62bd7
Mariam <mariansaad585@gmail.com>
2023-05-06 02:47:15 +0300

    log-in-service

--- frontend/src/app/auth/services/log-in/log-in.service.spec.ts
+++ frontend/src/app/auth/services/log-in/log-in.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import { LogInService } from './log-in.service';
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe('LogInService', () => {
+  let service: LogInService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    service = TestBed.inject(LogInService);
   });
 
   it('should be created', () => {
