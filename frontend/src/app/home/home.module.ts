03c9f11120b3471cb51b5f8ba4d4e792d6b5fceb
Omar Walid <omo_1712@yahoo.com>
2023-05-05 18:44:42 +0300

    fix router

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -4,20 +4,17 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
 import { LatestBlogComponent } from './components/latest-blog/latest-blog.component';
 import { TeamMemberComponent } from './components/team-member/team-member.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './home.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ContactUsComponent,
     LatestBlogComponent,
-    TeamMemberComponent,
     HeroComponent,
+    HomeComponent,
   ],
-  imports: [CommonModule],
-  exports: [
-    ContactUsComponent,
-    LatestBlogComponent,
-    TeamMemberComponent,
-    HeroComponent,
-  ],
+  imports: [CommonModule, SharedModule],
+  exports: [ContactUsComponent, LatestBlogComponent, HeroComponent],
 })
 export class HomeModule {}
