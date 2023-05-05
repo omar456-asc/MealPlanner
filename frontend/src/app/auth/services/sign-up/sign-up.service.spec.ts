4e40a30a2a5ecef122067cfc0dc933dc620d18d3
sara hussam <sarahussam203@gmail.com>
2023-05-06 02:32:00 +0300

    'login-service'

--- frontend/src/app/auth/services/sign-up/sign-up.service.spec.ts
+++ frontend/src/app/auth/services/sign-up/sign-up.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import { SignUpService } from './sign-up.service';
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe('SignUpService', () => {
+  let service: SignUpService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    service = TestBed.inject(SignUpService);
   });
 
   it('should be created', () => {
--- frontend/src/app/auth/services/sign-up/sign-up.service.ts
+++ frontend/src/app/auth/services/sign-up/sign-up.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class SignUpService {
 
   constructor() { }
 }
