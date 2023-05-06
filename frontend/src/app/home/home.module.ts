6b2f65974e5537516e420d9f90a41edf075733dc
nehad osman <nehad.osman19@gmail.com>
2023-05-06 21:58:35 +0300

    fix: slide issue

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { LatestBlogComponent } from './components/latest-blog/latest-blog.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { WhyUsComponent } from './components/why-us/why-us.component';
 
 
 @NgModule({
-  declarations: [ContactUsComponent, LatestBlogComponent, HeroComponent],
+  declarations: [ContactUsComponent, LatestBlogComponent, HeroComponent, WhyUsComponent],
   imports: [CommonModule],
-  exports: [ContactUsComponent, LatestBlogComponent, HeroComponent],
+  exports: [ContactUsComponent, LatestBlogComponent, HeroComponent, WhyUsComponent],
 })
 export class HomeModule {}
--- frontend/src/assets/js/main.js
+++ frontend/src/assets/js/main.js
@@ -74,25 +74,27 @@
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
-  let selectHeader = select("#header");
-  let selectTopbar = select("#topbar");
-  if (selectHeader) {
-    const headerScrolled = () => {
-      if (window.scrollY > 100) {
-        selectHeader.classList.add("header-scrolled");
-        if (selectTopbar) {
-          selectTopbar.classList.add("topbar-scrolled");
+  window.onload = setTimeout(function () {
+    let selectHeader = select("#header");
+    let selectTopbar = select("#topbar");
+    if (selectHeader) {
+      const headerScrolled = () => {
+        if (window.scrollY > 100) {
+          selectHeader.classList.add("header-scrolled");
+          if (selectTopbar) {
+            selectTopbar.classList.add("topbar-scrolled");
+          }
+        } else {
+          selectHeader.classList.remove("header-scrolled");
+          if (selectTopbar) {
+            selectTopbar.classList.remove("topbar-scrolled");
+          }
         }
-      } else {
-        selectHeader.classList.remove("header-scrolled");
-        if (selectTopbar) {
-          selectTopbar.classList.remove("topbar-scrolled");
-        }
-      }
-    };
-    window.addEventListener("load", headerScrolled);
-    onscroll(document, headerScrolled);
-  }
+      };
+      window.addEventListener("load", headerScrolled);
+      onscroll(document, headerScrolled);
+    }
+  }, 1000);
 
   /**
    * Back to top button
