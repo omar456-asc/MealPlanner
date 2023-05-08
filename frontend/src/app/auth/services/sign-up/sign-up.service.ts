87c234666ce25b58dfcf039ac5966e3455693804
Mariam <mariansaad585@gmail.com>
2023-05-08 14:08:55 +0300

    add sign-up servicein front branch

---
+++ frontend/src/app/auth/services/sign-up/sign-up.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignUpService {
+  private readonly SignupUrl = 'http://localhost:7400/api/users/create';
+
+  constructor(private readonly myUser:HttpClient) { }
+
+  AddNewUser(signupUser:any){
+    return this.myUser.post(this.SignupUrl, signupUser);
+  }
+}
