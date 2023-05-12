073ea2b383b18c90876b598cd15fa236669e89d4
nehad osman <nehad.osman19@gmail.com>
2023-05-12 21:11:57 +0300

    edit activated navbar & edit profile

--- frontend/src/assets/js/main.js
+++ frontend/src/assets/js/main.js
@@ -145,7 +145,17 @@
   /**
    * Mobile nav dropdowns activate
    */
-
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
