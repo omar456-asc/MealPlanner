64ec9561c41da5603a94e7cbf76c2b3ae1aa532b
Mariam <mariansaad585@gmail.com>
2023-05-15 21:40:04 +0300

    get cart

---
+++ frontend/src/app/checkout/service/shopping-cart.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingCartService {
+
+  private readonly Base_URL = "http://localhost:7400/api/cart";
+  constructor(private readonly myClient:HttpClient) { }
+//#region GetAllMeals
+  GetCart(id:any){
+    //method[Get-Delete-Put-Patch]
+
+    return this.myClient.get(this.Base_URL+"/"+id);
+  }
+
+}
