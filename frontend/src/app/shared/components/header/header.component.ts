38ebae98196188267537e0b96bbeeea5c55ae61f
sara hussam <sarahussam203@gmail.com>
2023-05-16 15:23:40 +0300

    adding size of cart in nav

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/log-in/auth.service';
+import { AllMealsService } from 'src/app/meals/services/all-meals.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,9 +9,21 @@ import { AuthService } from 'src/app/auth/services/log-in/auth.service';
 })
 export class HeaderComponent {
   isLoggedIn: any;
-  constructor(private authService: AuthService) {
+  public  cart: [{"id":string,"quantity":number}];
+  public oldcart: string | null;
+  cartLength: number;
+  constructor(private authService: AuthService,
+    private mymeals:AllMealsService) {
     console.log(this.authService.isUserLoggedIn());
     this.isLoggedIn = this.authService.isUserLoggedIn();
+    this.oldcart = localStorage.getItem('cart');
+    if (this.oldcart) {
+      this.cart = JSON.parse(this.oldcart);
+      this.cartLength=this.cart.length;
+    } else {
+      this.cart =[{"id":"0","quantity":0}];
+      this.cartLength=0
+    }
   }
   logout() {
     this.authService.logout();
