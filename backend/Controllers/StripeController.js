3081f11f8202d2812488c742524ba85e9cdf12fd
sara hussam <sarahussam203@gmail.com>
2023-05-16 19:40:36 +0300

    stripe backend

--- backend/Controllers/StripeController.js
+++ backend/Controllers/StripeController.js
@@ -1,26 +1,41 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+  
  var payment=(req, res) => {
-    const { amount, source, description } = req.body;
-  
+    // customer=createCustomer("Sarsour")
+    const { amount, name } = req.body;
     // Validate the payment information here
-  
-    stripe.charges.create({
+    
+   const  customer=   stripe.customers.create({
+    
+      name: name,
+      source: "tok_visa"
+    }).then( customer => {
+        
+    return  stripe.charges.create({
       amount: amount,
       currency: 'USD',
-      source: source,
-      description: description
-    }, (err, charge) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Payment failed');
-      } else {
+      customer: customer.id,
+      description: "Order Payment Success"});
+      
+    }).then( charge=> {
+       
+    
         console.log(charge);
         res.status(200).send('Payment successful');
-      }
+       
+      }).catch(err => {
+        
+        
+        console.error(err);
+        res.status(500).send('Payment failed');
+      
     });
 }
 module.exports ={
     payment
+    
 }
