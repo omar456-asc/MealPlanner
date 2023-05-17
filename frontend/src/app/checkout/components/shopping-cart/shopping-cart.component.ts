65ea1a7e25f2f10922d38304860cd9bfea14b8b5
Omar Walid <omo_1712@yahoo.com>
2023-05-17 23:20:34 +0300

    fix nested loops

--- frontend/src/app/checkout/components/shopping-cart/shopping-cart.component.ts
+++ frontend/src/app/checkout/components/shopping-cart/shopping-cart.component.ts
@@ -8,45 +8,36 @@ import { AllMealsService } from 'src/app/meals/services/all-meals.service';
   styleUrls: ['./shopping-cart.component.css'],
 })
 export class ShoppingCartComponent implements OnInit {
-
   quantityInput = 1;
-  ID:any=localStorage.getItem('id')
-  localcart:any =localStorage.getItem('cart')
-  cartid:any=[]
-  Meal:any=[]
+  ID: any = localStorage.getItem('id');
+  localcart: any = localStorage.getItem('cart');
+  cartid: any = [];
+  Meal: any = [];
   constructor(
-
     public myService: ShoppingCartService,
-    public mymeals: AllMealsService,){}
+    public mymeals: AllMealsService
+  ) {}
   ngOnInit(): void {
-    this.cartid=JSON.parse(this.localcart)
-    for (let i=0; i<this.cartid.length; i++){
-      this.mymeals.GetMealByID(this.cartid[i].id).subscribe(
-        {
-          next:(data:any)=>{
-            //  console.log(data);
-            this.Meal.push(data);
-
-
-          },
-          error:(err)=>{console.log(err)}
-        }
-      );
-
+    this.cartid = JSON.parse(this.localcart);
+    for (let i = 0; i < this.cartid.length; i++) {
+      this.mymeals.GetMealByID(this.cartid[i].id).subscribe({
+        next: (data: any) => {
+          console.log(data);
+          this.Meal.push(data);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
     }
-
-    console.log()
-
-
   }
-  checkout(){
-    this.myService.AddToUserCart(this.cartid,this.ID).subscribe((data:any)=>{
-
-},
-(err)=>{
-  // this.router.navigateByUrl('login');
-}
-);
+  checkout() {
+    this.myService.AddToUserCart(this.cartid, this.ID).subscribe(
+      (data: any) => {},
+      (err) => {
+        // this.router.navigateByUrl('login');
+      }
+    );
   }
 
   minus() {
