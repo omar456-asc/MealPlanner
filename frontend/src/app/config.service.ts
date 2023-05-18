705674c9fb5a063933efa768c22df1953d0cdfb0
Omar Walid <omo_1712@yahoo.com>
2023-05-19 02:01:43 +0300

    put deploy url

--- frontend/src/app/config.service.ts
+++ frontend/src/app/config.service.ts
@@ -4,7 +4,7 @@ import { Injectable, inject } from '@angular/core';
   providedIn: 'root',
 })
 export class ConfigService {
-  private baseUrl = 'http://localhost:7400/api';
+  private baseUrl = 'https://mealplannerapi.onrender.com/api';
   static getBaseUrl: any;
   constructor() {}
   getBaseUrl(endpoint: string): string {
