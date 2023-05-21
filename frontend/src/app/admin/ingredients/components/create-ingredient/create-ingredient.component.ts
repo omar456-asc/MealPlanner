1ea17d83871cd6aea245ce0052ae58a4608a3dc0
Omar Walid <omo_1712@yahoo.com>
2023-05-21 14:51:43 +0300

    create ingredient form

--- frontend/src/app/admin/ingredients/components/create-ingredient/create-ingredient.component.ts
+++ frontend/src/app/admin/ingredients/components/create-ingredient/create-ingredient.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-ingredient',
   templateUrl: './create-ingredient.component.html',
-  styleUrls: ['./create-ingredient.component.css']
+  styleUrls: ['./create-ingredient.component.css'],
 })
-export class CreateIngredientComponent {
+export class CreateIngredientComponent implements OnInit {
+  public ingredient:
+    | [
+        {
+          id: string;
+          imgae: string;
+          consistency: any;
+          name: string;
+          price: number;
+        }
+      ]
+    | undefined;
 
+  constructor() {
+    this.ingredient = [
+      { id: '0', imgae: '', consistency: '', name: '', price: 0 },
+    ];
+  }
+  ngOnInit(): void {}
+  createIngredient() {
+    console.log('ingredeint');
+  }
 }
